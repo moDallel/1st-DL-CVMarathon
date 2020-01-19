6cb5520ff2e72904b3ec71aa80a26a87ab6184e3
double1010x2 <double1010x2@gmail.com>
2020-01-19 17:25:47 +0800

    [Day - 018] HW: Day_018_HW.py Note: Vgg16 model

---
+++ homework/day018/Day_018_HW.py
@@ -0,0 +1,102 @@
+# %%
+'''
+## 『本次練習內容』
+#### 學習如何搭建Vgg16模型
+'''
+
+# %%
+'''
+## 『本次練習目的』
+  #### 熟悉經典Vgg模型的搭建
+  #### 時間較為充裕的學員可以嘗試搭建Vgg19當作練習
+'''
+
+# %%
+'''
+---
+'''
+
+# %%
+'''
+##### 請參考下面結構圖搭建Vgg16模型
+'''
+
+# %%
+'''
+![vgg16](VGG16_p.png)
+'''
+
+# %%
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras import backend as K
+
+
+
+def VGG16(include_top=True,input_tensor=None, input_shape=(224,224,1),
+          pooling='max',classes=1000):
+
+    img_input = Input(shape=input_shape)
+
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    '''可參考上面的搭法'''
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    '''可參考上面的搭法'''
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    '''可參考上面的搭法'''
+    if include_top:
+        # Classification block
+        x = Flatten(name='flatten')(x)
+        x = Dense(4096, activation='relu', name='fc1')(x)
+        x = Dense(4096, activation='relu', name='fc2')(x)
+        x = Dense(classes, activation='softmax', name='predictions')(x)
+    else:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+    inputs = img_input
+    # Create model.
+    model = Model(inputs, x, name='vgg16')
+
+
+    return model
+
+
+
+# %%
+model = VGG16(include_top=False)
+
+# %%
+model.summary()
